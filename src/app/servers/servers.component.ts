9bd94cd4333816d90086ae7699511ba8b051f41c
WillAnjos <willanjos@hotmail.com>
2019-10-24 17:07:41 -0300

    Two way databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,29 @@ import { Component, OnInit } from "@angular/core";
   // selector: "[app-servers]",
   // selector: ".app-servers",
   selector: "app-servers",
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: "./servers.component.html",
   styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  severCreationStatus = "No server was created!";
+  serverName = "Testserver";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {}
+
+  onCreateServer() {
+    this.severCreationStatus = "Server was created!";
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
